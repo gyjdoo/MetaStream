7ad7029b13685633b225d95c0cd5d39cd3c2ae95
Yongjie Guan <gyjdoo@gmail.com>
2022-07-10 19:12:21 -0400

    Add files via upload

---
+++ Installation/installVScode.sh
@@ -0,0 +1,3 @@
+VERSION=latest
+wget -N -O vscode-linux-deb.arm64.deb https://update.code.visualstudio.com/$VERSION/linux-deb-arm64/stable
+sudo apt install ./vscode-linux-deb.arm64.deb
---
+++ Installation/runFan.sh
@@ -0,0 +1,2 @@
+sudo /usr/bin/jetson_clocks
+sudo sh -c 'echo 255 > /sys/devices/pwm-fan/target_pwm'
